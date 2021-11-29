b3cb62c8da48e0ef96ce39cd253ca88854bad321
tundvgs <79246223+tundvgs@users.noreply.github.com>
2021-11-29 18:27:40 +0700

    update response base

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,8 @@ import { CatsModule } from './cats/cats.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { TransformInterceptor } from './interceptor/transform.interceptor';
 
 @Module({
   imports: [
@@ -19,7 +21,12 @@ import configuration from './config/configuration';
     }),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
